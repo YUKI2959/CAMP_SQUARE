183f132fc5bc7a3d1595df2e72dcf5f6112d694b
内田有貴 <117145436+YUKI2959@users.noreply.github.com>
2022-12-19 17:21:10 +0900

    ビューを見やすく修正+リファクタリング

--- app/models/submit.rb
+++ app/models/submit.rb
@@ -1,4 +1,10 @@
 class Submit < ApplicationRecord
+
+  validates :title, presence: true
+  validates :content, presence: true
+  validates :image, presence: true
+
+
   has_one_attached :image
   belongs_to :user
 end
