d7596b37c20d76713ba75d327c76de33dabe6de6
内田有貴 <117145436+YUKI2959@users.noreply.github.com>
2022-12-19 15:35:52 +0900

    新規テーマ投稿機能追加

--- app/controllers/submits_controller.rb
+++ app/controllers/submits_controller.rb
@@ -1,4 +1,29 @@
 class SubmitsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
+
   def index
+  end
+
+  def new
+    @submit = Submit.new
+  end
+
+  def create
+    @submit = Submit.new(submit_params)
+    if @submit.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def submit_params
+    params.require(:submit).permit(:title, :content, :image).merge(user_id: current_user.id)
+  end
+
+  def contributor_confirmation
+    redirect_to root_path unless current_user == @submit.user
   end
 end
