4508c7bed1b1cfa5903a485b30accc93fd47dff9
内田有貴 <117145436+YUKI2959@users.noreply.github.com>
2022-12-20 12:08:17 +0900

    フォロー&解除機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
+
+  get 'relationships/followings'
+  get 'relationships/followers'
+
   devise_for :users
+
   root to: "submits#index"
-  resources :users, only: [:edit, :update, :show]
+
+  resources :users, only: [:edit, :update, :show]do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
   resources :submits, only: [:new, :create, :destroy, :show, :edit, :update] do
     resources :comments, only: :create
   end
