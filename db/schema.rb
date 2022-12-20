3dda7a79bb80b8ab4d6c2a8a12f6ef3209747168
内田有貴 <117145436+YUKI2959@users.noreply.github.com>
2022-12-20 21:50:46 +0900

    人気ランキング表示

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_20_065203) do
+ActiveRecord::Schema.define(version: 2022_12_20_065429) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -82,6 +82,7 @@ ActiveRecord::Schema.define(version: 2022_12_20_065203) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "impressions_count", default: 0
     t.index ["user_id"], name: "index_submits_on_user_id"
   end
 
