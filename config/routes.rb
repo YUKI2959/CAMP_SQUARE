7110e5fa459ec97c2154bd9979ee2764177e44eb
内田有貴 <117145436+YUKI2959@users.noreply.github.com>
2022-12-20 10:55:29 +0900

    ユーザー詳細ページ追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "submits#index"
-  resources :users, only: [:edit, :update]
+  resources :users, only: [:edit, :update, :show]
   resources :submits, only: [:new, :create, :destroy, :show, :edit, :update] do
     resources :comments, only: :create
   end
