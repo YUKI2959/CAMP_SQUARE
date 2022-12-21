fa0c481de64281bca44332ecbd10823bc63907fb
内田有貴 <117145436+YUKI2959@users.noreply.github.com>
2022-12-21 15:45:05 +0900

    プロフ画像導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
 
   private
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:profile, :avatar])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:profile,:image])
   end
 
   def basic_auth
