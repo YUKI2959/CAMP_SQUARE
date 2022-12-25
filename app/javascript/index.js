f36debf9b170b98e28c6172a7dda56a6b6dc6710
内田有貴 <117145436+YUKI2959@users.noreply.github.com>
2022-12-25 10:51:22 +0900

    プロフ画面からプルダウンを出すように変更&トップ画面修正

---
+++ app/javascript/index.js
@@ -0,0 +1,14 @@
+window.addEventListener('load', function(){
+
+  const pullDownButton = document.getElementById("lists")
+  const pullDownParents = document.getElementById("pull-down")
+
+  pullDownButton.addEventListener('click', function() {
+    // プルダウンメニューの表示と非表示の設定
+    if (pullDownParents.getAttribute("style") == "display:block;") {
+      pullDownParents.removeAttribute("style")
+    } else {
+      pullDownParents.setAttribute("style", "display:block;")
+    }
+  })
+})
